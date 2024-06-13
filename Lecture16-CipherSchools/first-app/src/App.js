45e98c5ce48a40ad99c0e17a61a60f300f1c5a7b
sanchit bajaj <sanchitbajaj2003@gmail.com>
2024-06-13 17:51:13 +0530

    final

---
+++ Lecture16-CipherSchools/first-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name='sanchit'
+  return (
+
+<>
+<h1> hello {name}!</h1>
+<div>
+<centre>
+<h2 style={{color:"red"}}>WELCOME TO CIPHER SCHOOL</h2>
+</centre>
+</div>
+</>
+
+  );
+}
+
+export default App;
